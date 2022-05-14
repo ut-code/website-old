cae6cc9fed1e60eb8fe1d6016e326ebc03de70de
kazukinumazato <eigobaka@icloud.com>
2022-05-14 13:45:47 +0900

    トップページの大幅な変更

---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import AnchorButton from "./AnchorButton";
+import Logo from "./Logo";
+
+export default function Header() {
+  return (
+    <header className="flex mt-12">
+      <a href="/">
+        <Logo variant="light" className="md:hidden w-32 sm:w-48" />
+        <Logo variant="default" className="hidden md:block w-48" />
+      </a>
+      <nav className="ml-auto">
+        <ul className="list-none flex items-center">
+          <li className="mx-8 text-white text-lg">
+            <a href="/">ブログ</a>
+          </li>
+          <li className="mx-8 text-white text-lg">
+            <a href="/">活動理念</a>
+          </li>
+          <li className="mx-8">
+            <AnchorButton variant="medium" href="/">
+              お問い合わせ
+            </AnchorButton>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,9 @@ import conceptImage from "../../resources/concept.svg";
 import homeGoalLearningImage from "../../resources/home-goal-learning.svg";
 import homeGoalCommunityImage from "../../resources/home-goal-community.svg";
 import homeGoalDevelopmentImage from "../../resources/home-goal-development.svg";
-import Logo from "../components/Logo";
 import AnchorButton from "../components/AnchorButton";
 import ContentContainer from "../components/ContentContainer";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -32,10 +32,7 @@ export default function Home() {
           </div>
         </div>
         <ContentContainer className="flex flex-col relative h-full">
-          <header className="flex mt-12">
-            <Logo variant="light" className="md:hidden w-32 sm:w-48" />
-            <Logo variant="default" className="hidden md:block w-48" />
-          </header>
+          <Header />
           <div className="flex-grow md:flex items-center mt-8 md:mt-0">
             <div className="flex-grow">
               <div className="text-3xl sm:text-5xl sm:leading-tight md:text-3xl md:leading-normal lg:text-5xl lg:leading-tight xl:text-6xl xl:leading-tight font-extrabold">
@@ -49,7 +46,7 @@ export default function Home() {
                 </span>
                 <span className="text-white md:text-black">コミュニティ</span>
               </div>
-              <AnchorButton variant="large" href="/" className="mt-6">
+              <AnchorButton variant="large" href="/sample" className="mt-6">
                 JOIN US
               </AnchorButton>
             </div>
@@ -62,28 +59,42 @@ export default function Home() {
         </ContentContainer>
       </div>
       <ContentContainer>
+        <p className="text-center mt-40 text-lg">
+          ut.code();のウェブサイトへようこそ！
+        </p>
+        <p className="text-center text-lg">
+          私たちは東京大学のソフトウェアエンジニアリングサークルです！
+        </p>
+        <div className="text-center mt-20 text-5xl font-extrabold">
+          私たちの<span className="text-green-400">活動</span>
+        </div>
         <div className="lg: grid grid-cols-3 gap-12 max-w-screen-xl mx-auto">
           {[
             {
               title: "学習",
               image: homeGoalLearningImage,
-              description: "吾輩は猫である",
+              description:
+                "私たちはコミュニティのメンバーがプログラミングの技術の習得を行えるような活動を行います。例えば各プログラミング言語やソフトウェア技術に関する講習会を開き、プログラミングに関する技術を教えます。また、コミュニティのメンバーが学習を進める上で、指針となりうるような教材の作成を行います。さらに、ハッカソンを通じて実践的な開発技術の習得を目指します。",
             },
             {
               title: "交流",
               image: homeGoalCommunityImage,
-              description: "吾輩は猫である",
+              description:
+                "大規模なプロジェクトを行ううえで、技術者同士の交流があることは欠かせません。私たちはコミュニティのメンバーが互いに親睦を深め、唯一無二の関係を構築できる場を提供します。様々な開発をチームに分かれて行いチーム内での結束力を高めることで、将来的に様々な場面でかかわり開発を行えるような関係の構築を目指します。",
             },
             {
               title: "開発",
               image: homeGoalDevelopmentImage,
-              description: "開発を行います",
+              description:
+                "私たちはコミュニティのメンバーがプログラミングの技術の習得を行えるような活動を行います。例えば各プログラミング言語やソフトウェア技術に関する講習会を開き、プログラミングに関する技術を教えます。また、コミュニティのメンバーが学習を進める上で、指針となりうるような教材の作成を行います。さらに、ハッカソンを通じて実践的な開発技術の習得を目指します。",
             },
           ].map(({ title, image, description }) => (
-            <div key={title}>
-              <div>{title}</div>
+            <div key={title} className="mt-10">
+              <div className="text-center font-extrabold text-4xl">{title}</div>
               <Image layout="responsive" src={image} />
-              <div>{description}</div>
+              <div className="mt-20 leading-loose tracking-widest text-xl">
+                {description}
+              </div>
             </div>
           ))}
         </div>
